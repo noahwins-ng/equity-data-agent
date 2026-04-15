e80b408d34ca8a7984a5f0eddcfbec965a8bbe9e
noahwins-ng <noahwins.dev@gmail.com>
2026-04-15 18:06:13 +0800

    QNT-42: feat(dagster): add fundamentals asset with yfinance ingestion to ClickHouse

--- packages/dagster-pipelines/src/dagster_pipelines/assets/__init__.py
+++ packages/dagster-pipelines/src/dagster_pipelines/assets/__init__.py
@@ -1,3 +1,4 @@
+from dagster_pipelines.assets.fundamentals import fundamentals
 from dagster_pipelines.assets.ohlcv_raw import ohlcv_raw
 
-__all__ = ["ohlcv_raw"]
+__all__ = ["fundamentals", "ohlcv_raw"]
--- packages/dagster-pipelines/src/dagster_pipelines/definitions.py
+++ packages/dagster-pipelines/src/dagster_pipelines/definitions.py
@@ -1,10 +1,11 @@
 from dagster import Definitions
 
+from dagster_pipelines.assets.fundamentals import fundamentals
 from dagster_pipelines.assets.ohlcv_raw import ohlcv_raw
 from dagster_pipelines.resources.clickhouse import ClickHouseResource
 
 defs = Definitions(
-    assets=[ohlcv_raw],
+    assets=[ohlcv_raw, fundamentals],
     resources={
         "clickhouse": ClickHouseResource(),
     },
